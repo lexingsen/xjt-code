9d5207a6263284047fbb2d227578ba139b76f92f
lexingsen <1050592374@qq.com>
2020-12-05 13:52:46 +0800

    add 先序遍历

--- Tree/noRecursionPreOrder.cc
+++ Tree/noRecursionPreOrder.cc
@@ -12,51 +12,6 @@ struct TreeNode {
   TreeNode *left, *right;
 };
 
-
-vector<int> preOrder(TreeNode *root) {
-  vector<int> res;
-  if (!root) return res;
-  auto p = root;
-  stack<TreeNode*> st;
-  while (st.size() || p) {
-    if (p) {
-      st.push(p);
-      res.push_back(p->val);
-      p = p->left;
-    } else {
-      auto top = st.top(); st.pop();
-      p = top->right;
-    }
-  }
-  return res;
-}
-
-
-vector<int> postOrder(TreeNode *root) {
-  vector<int> res;
-  if (!root) return res;
-  stack<TreeNode*> st;
-  auto p = root;
-  TreeNode *tag = nullptr;
-  while (st.size() || p) {
-    while (p) {
-      st.push(p);
-      p = p->left;
-    }
-    p = st.top(); st.pop();
-    if (!p->right || p->right==tag) {
-      res.push_back(p->val);
-      tag = p;
-      p = nullptr;
-    } else {
-      st.push(p);
-      p = p->right;
-    }
-  }
-  return res;
-}
-
-
 vector<int> inOrder(TreeNode *root) {
   vector<int> res;
   if (!root) return res;
